e094376de3c7406a5f92e516366ae6595baf99e6
BFFBFFSolutions <team@bffsolution.kz>
2025-02-21 17:29:37 +0500

    add playbooks

---
+++ files/gitlab-runner-register.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+# modifier avec votre token
+registration_token= 
+url= 
+
+docker exec -it gitlab-runner1 \
+  gitlab-runner register \
+    --non-interactive \
+    --registration-token ${registration_token} \
+    --locked=false \
+    --description docker-stable \
+    --url ${url} \
+    --executor docker \
+    --docker-image docker:stable \
+    --docker-volumes "/var/run/docker.sock:/var/run/docker.sock"
+    
+# executer le script pour inscrire le runner dans Gitlab
